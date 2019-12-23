5c872fe27594ee6912ac3cc9c240a45596387697
unknown <vasilij.1357@yandex.ru>
2019-12-23 23:29:03 +0300

    Удалили из HTML файл с нативным JS и добавили файл с JQuery

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,14 @@
+// document.getElementById("button-for-modal").onclick = function() {
+//   document.getElementById("modal").classList.add("modal__active");
+// };
+
+// document.getElementById("close-modal").onclick = function() {
+//   document.getElementById("modal").classList.remove("modal__active");
+// };
+
 document.getElementById("button-for-modal").onclick = function() {
   document.getElementById("modal").classList.add("modal__active");
-};
-
-document.getElementById("close-modal").onclick = function() {
-  document.getElementById("modal").classList.remove("modal__active");
+  setTimeout(() => {
+    document.getElementById("modal").classList.remove("modal__active");
+  }, 5000);
 };

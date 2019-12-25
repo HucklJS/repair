a49db2f015c87a0774962883c156388b3d30652e
unknown <vasilij.1357@yandex.ru>
2019-12-25 19:39:14 +0300

    Добавили блок price и подключили animate.css и wow.js для анимации.

--- src/js/startOwnCaroisel.js
+++ src/js/startOwnCaroisel.js
@@ -1,3 +1,28 @@
-$(document).ready(function() {
-  $(".owl-carousel").owlCarousel();
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  slideBy: 1,
+  //   autoWidth: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    768: {
+      items: 2
+    },
+    1200: {
+      items: 3
+    }
+  }
+});
+
+$(".arrows__right").click(function() {
+  $(".owl-carousel").trigger("next.owl.carousel", [600]);
+});
+// Go to the previous item
+$(".arrows__left").click(function() {
+  // With optional speed parameter
+  // Parameters has to be in square bracket '[]'
+  $(".owl-carousel").trigger("prev.owl.carousel", [600]);
 });
